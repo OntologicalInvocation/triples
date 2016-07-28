2cb88ac14539e280a264c82e15008a3fb8f20597
Bill La Forge <laforge49@gmail.com>
2016-07-28 10:11:25 -0400

    project.cli

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject
+  boot-project
+  "0.0.0-SNAPSHOT"
+  :dependencies
+  [[org.clojure/clojure "1.9.0-alpha10" :scope "provided"]
+   [adzerk/boot-test "1.1.2" :scope "test"]
+   [org.clojure/core.async "0.2.385"]]
+  :source-paths
+  [])
