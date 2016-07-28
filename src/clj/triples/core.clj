0441de117a7cc2afcd482486276155061b2ac3a3
Bill La Forge <laforge49@gmail.com>
2016-07-28 11:31:29 -0400

    using defmethod

--- src/clj/triples/core.clj
+++ src/clj/triples/core.clj
@@ -3,3 +3,15 @@
 (def x 100)
 
 (defrecord semtriple [subj pred obj])
+
+(defn semtriple-str
+  [st]
+  (str "\n" ::semtriple
+       "\n   " :subj " " (:subj st)
+       "\n   " :pred " " (:pred st)
+       "\n   " :obj " " (:obj st)
+       ))
+
+(defmethod print-method semtriple
+  [semtriple writer]
+  (print-simple (semtriple-str semtriple) writer))
